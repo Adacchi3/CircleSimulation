00b4a782317f42455a1b2ed969baf1073c0a7036
Adacchi3 <adachi.prog@gmail.com>
2017-12-14 01:12:54 +0900

    add the definition of param on init

--- model/CirclePaticipant.py
+++ model/CirclePaticipant.py
@@ -1,17 +1,26 @@
 import numpy as np
 import math
 
-class GeneralParticipant:
+class CircleParticipant:
     
     def __init__(self, id):
         self.id = id
-        self.budget = 0
-        while( self.budget <= 0):
-            self.budget = math.floor(np.random.normal(3851.35,3641.7))
-        self.start = np.random.randint(71)
-        self.time = np.random.randint(71)
-        self.alpha = np.random.rand()
-        self.beta = 1 - self.alpha
-        self.log = []
-        self.circlelist = []
+        self.bookfee = 0
+        while( self.bookfee <= 0):
+            self.bookfee = math.floor(np.random.normal(30757.57,29924.93))
+        #self.book_quality = 0.5 + (self.bookfee-30757.57)/(3*29924.93)
+        self.book_quality = np.random.normal(0.5,0.2)
+        self.ad = np.random.rand()
+        self.dm = np.random.rand()
+        self.category = np.random.randint(47)
+        self.price = math.floor(np.random.normal(596,382.15))
 
+"""
+if __name__ == '__main__':
+    agent = CircleParticipant(1)
+    print(agent.id)
+    print(agent.price)
+    print(agent.ad)
+    print(agent.dm)
+    print(agent.book_quality)
+"""
