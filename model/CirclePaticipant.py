653ff0bb9c44bf065f01fbd62a083d44586b766e
Adacchi3 <adachi.prog@gmail.com>
2017-12-13 23:51:35 +0900

    create the Circle paticipant file

---
+++ model/CirclePaticipant.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+
+class GeneralParticipant:
+    
+    def __init__(self, id):
+        self.id = id
+        self.budget = 0
+        while( self.budget <= 0):
+            self.budget = math.floor(np.random.normal(3851.35,3641.7))
+        self.start = np.random.randint(71)
+        self.time = np.random.randint(71)
+        self.alpha = np.random.rand()
+        self.beta = 1 - self.alpha
+        self.log = []
+        self.circlelist = []
+
