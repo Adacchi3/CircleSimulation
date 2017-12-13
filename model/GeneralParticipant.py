9cf27cc7d11e412341b37ca04444855a616706aa
Adacchi3 <adachi.prog@gmail.com>
2017-12-14 02:30:12 +0900

    remove spellmiss file and add check_ad function on GeneralParticipant

--- model/GeneralParticipant.py
+++ model/GeneralParticipant.py
@@ -1,3 +1,4 @@
+from CircleParticipant import *
 import numpy as np
 import math
 
@@ -21,6 +22,21 @@ class GeneralParticipant:
         str =  "{{\'id\':{0.id}, \'budget\': {0.budget}, \'money\': {0.money}, \'start\': \'{0.start}, \'time\': \'{0.time}\' \'alpha\': {0.alpha}, \'beta\': {0.beta}, \'like_category\': {0.like_category}, \'log\': {0.log}, \'circlelist\': {0.circlelist}}}".format(self)
         return str
 
+    def check_ad(self, circles):
+        for i in range(0, len(circles)):
+            if(np.random.rand()<0.1):
+                self.circlelist.append(circles[i].id)
+        return
+
+"""
 if __name__ == '__main__':
     agent = GeneralParticipant(1)
+
+    circles = []
+    i = 0
+    for i in range(100):
+        circles.append(CircleParticipant(i))
+    print(circles)
+    agent.check_ad(circles)
     print(agent.show_info())
+"""
